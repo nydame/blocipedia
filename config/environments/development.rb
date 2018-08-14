df8ad98cdc3ebfa8665cccd0a30b41d4893c06f2
felicia <felicia@hyperarts.com>
2018-08-14 11:36:18 -0700

    Stuck on email confirmation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,20 @@ Rails.application.configure do
   # Set default URL for mailer
   config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
 
+  # Set up for mail delivery
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  enable_starttls_auto: true,
+  user_name: ENV["gmail_username"],
+  password:  ENV["gmail_password"],
+  authentication: :plain,
+  domain: 'gmail.com'
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
