d1ed1c602c6b6574f1a46b92b86aae5b220bd49b
felicia <felicia@hyperarts.com>
2018-08-26 15:09:43 -0700

    Checkpoint 6 - Seeding Data: completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'random_data'
+
+# Create users at standard level
+5.times do |index|
+  User.create!(
+    username: Faker::DragonBall.character,
+    email: "user-#{index}@bloc.com",
+    password: RandomData.random_password,
+  )
+end
+
+# Create 1 admin user with known properties
+User.create!(
+  username: "adminuser",
+  email: "admin1@bloc.com",
+  password: "adminOne!",
+  role: :admin
+)
+
+# Create 1 premium user with known properties
+User.create!(
+  username: "premiumuser",
+  email: "premium1@bloc.com",
+  password: "premiumOne!",
+  role: :premium
+)
+
+users = User.all
+
+# Create wikis
+10.times do
+  Wiki.create!(
+    title: Faker::Myst.game,
+    body: Faker::HitchhikersGuideToTheGalaxy.quote,
+    private: [true, false, false].sample, #skew in favor of public wikis
+    user: users.sample
+  )
+end
+
+puts "Seeding finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
