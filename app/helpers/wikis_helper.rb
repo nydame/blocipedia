7dabbe49ff695c283533740929ba91846fa5b98e
felicia <felicia@hyperarts.com>
2018-09-14 20:09:14 -0700

    Checkpoint 10 - Wiki Collaborators: completed with collaborator deletion implemented.

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -20,6 +20,12 @@ module WikisHelper
     User.joins(:collaborations).where({collaborations: {wiki_id: wiki.id}}).pluck(:id, :username)
   end
 
+  def collaboration_from_user(uid, wid)
+    #return a number
+    c = Collaboration.where(user_id: uid, wiki_id: wid).pluck(:id)
+    return c[0] unless c.count < 1
+  end
+
   def render_markdown_as_html(wiki_body)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, tables: true, fenced_code_blocks: true, autolink: true, disable_indented_code_blocks: true, superscript: true)
     markdown.render(wiki_body)
